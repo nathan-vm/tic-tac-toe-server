de19348096204b1b005fc4cd872eef216ee9b39f
Nathan Vieira <nathanvieira@lett.digital>
2020-11-22 23:10:01 -0300

    Refactored server.js

--- src/server.js
+++ src/server.js
@@ -2,128 +2,178 @@ import dotenv from 'dotenv'
 import http from 'http'
 import { v4 as uuid } from 'uuid'
 
-import { gameRules } from './gameRules.js'
 import { Server } from 'socket.io'
 dotenv.config()
 
 const PORT = process.env.PORT
 const HOST = process.env.HOST
 
-let players = []
-let sockets = []
-const games = []
+const players = {
+  /**
+   * nathan: {
+   *  win: 10,
+   *  draw: 5,
+   *  played: 20,
+   * },
+   * mauricius: {
+   *  win: 10,
+   *  draw: 5,
+   *  played: 20,
+   * },
+   * cadu: {
+   *  win: 10,
+   *  draw: 5,
+   *  played: 20,
+   * },
+   */
+}
+const sockets = {
+  /**
+   *
+   * y4JZOz9g5ma9k1PJAAAB : {
+   *  name: Mauricius,
+   *  gameId: null,
+   *  isPlaying: false
+   * },
+   *
+   * dFlua5hkgjA2K3NSAAAD : {
+   *  name: Nathan,
+   *  gameId: 2189a7bb-709a-46f7-9ff9-3b9c55e6543c,
+   *  isPlaying: true
+   * },
+   *
+   * 7oBVk0gIvaALKYmnAAAF : {
+   *  name: Cadu,
+   *  gameId: 2189a7bb-709a-46f7-9ff9-3b9c55e6543c,
+   *  isPlaying: true
+   * }
+   */
+}
+const games = {
+  /**
+   * {
+   *    2189a7bb-709a-46f7-9ff9-3b9c55e6543c: {
+   *      player1: dFlua5hkgjA2K3NSAAAD,
+   *      player2: 7oBVk0gIvaALKYmnAAAF,
+   *      whoseTurn: dFlua5hkgjA2K3NSAAAD,
+   *      sign: {
+   *        dFlua5hkgjA2K3NSAAAD: 'X',
+   *        7oBVk0gIvaALKYmnAAAF: 'O',
+   *      },
+   *      playboard: [
+   *        ['','','']
+   *        ['','','']
+   *        ['','','']
+   *      ],
+   *      gameStatus: 'ongoing', // "ongoing","won","draw"
+   *      gameWinner: null, // player id if status won
+   *    }
+   * },
+   */
+}
 
 const server = http.createServer()
-const io = new Server(server, { cors: ['http:localhost:3000'] })
+const io = new Server(server, { cors: ['http://localhost:8080'] })
 
 io.on('connection', client => {
   console.log('connected : ' + client.id)
+
   client.emit('connected', { id: client.id })
 
-  client.on('checkUserDetail', data => {
-    const alreadyExist = sockets.find(socket => {
-      if (socket.id === data.id) {
-        return true
+  client.on('Login', data => {
+    let socketVerification = false
+    let response = null
+
+    for (const id in sockets) {
+      if (sockets[id].name === data.name) {
+        socketVerification = true
+        response = sockets[id]
       }
-      return false
-    })
+    }
 
-    let response = alreadyExist
-
-    if (!alreadyExist) {
-      const newSocket = {
-        id: client.id,
-        isPlaying: false,
+    if (!socketVerification) {
+      sockets[client.id] = {
+        name: data.name,
         gameId: null,
-        playerData: null,
+        isPlaying: false,
       }
 
-      sockets = [...sockets, newSocket]
+      let alreadyPlayed = false
 
-      const alreadyPlayed = players.find(player => {
-        if (player.name === data.name) {
-          return true
+      for (const name in players) {
+        if (name === data.name) {
+          alreadyPlayed = true
         }
-        return false
-      })
-
-      newSocket.playerData = alreadyPlayed
+      }
 
       if (!alreadyPlayed) {
-        const newPlayer = {
-          id: client.id,
-          name: data.name,
+        players[data.name] = {
           played: 0,
           won: 0,
           draw: 0,
         }
-
-        players = [...players, newPlayer]
-
-        newSocket.playerData = newPlayer
       }
 
-      response = newSocket
+      response = sockets[client.id]
     }
-    client.emit('checkUserDetailResponse', response)
+    client.emit('LoginResponse', response)
   })
 
   client.on('getOpponents', () => {
+    console.log(sockets[client.id])
+
     const response = []
-    sockets.forEach(socket => {
-      if (socket.id !== client.id && !socket.isPlaying) {
+
+    for (const id in sockets) {
+      if (id !== client.id && !id.isPlaying) {
         response.push({
-          id: socket.id,
-          name: socket.playerData.name,
-          played: socket.playerData.played,
-          won: socket.playerData.won,
-          draw: socket.playerData.draw,
+          id: id,
+          name: sockets[id].name,
+          played: players[sockets[id].name].played,
+          won: players[sockets[id].name].won,
+          draw: players[sockets[id].name].draw,
         })
       }
-    })
+    }
 
     client.emit('getOpponentsResponse', response)
 
-    const clientSocket = sockets.find(({ id }) => id === client.id)
-
-    if (clientSocket) {
-      client.broadcast.emit('newOpponentAdded', {
-        id: clientSocket.id,
-        name: clientSocket.playerData.name,
-        played: clientSocket.playerData.played,
-        won: clientSocket.playerData.won,
-        draw: clientSocket.playerData.draw,
-      })
-    }
+    client.broadcast.emit('newOpponent', {
+      id: client.id,
+      name: sockets[client.id].name,
+      played: players[sockets[client.id].name].played,
+      won: players[sockets[client.id].name].won,
+      draw: players[sockets[client.id].name].draw,
+    })
   })
 
   client.on('selectOpponent', data => {
-    const player2Socket = sockets.find(
-      ({ id, isPlaying }) => data.id === id && !isPlaying,
-    )
+    let player1 = null
+    let player2 = null
+    if (sockets[data.id] && !sockets[data.id]?.isPlaying) {
+      player2 = sockets[data.id]
+    }
 
-    if (player2Socket) {
-      const player1Socket = sockets.find(({ id }) => client.id === id)
-
+    if (player2) {
       const gameId = uuid()
+      player1 = sockets[client.id]
 
-      player1Socket.is_playing = true
-      player2Socket.is_playing = true
+      player1.isPlaying = true
+      player2.isPlaying = true
 
-      player1Socket.gameId = gameId
-      player2Socket.gameId = gameId
+      player1.gameId = gameId
+      player2.gameId = gameId
 
-      player1Socket.played += 1
-      player2Socket.played += 1
+      player1.played += 1
+      player2.played += 1
 
-      const gameData = {
-        gameId,
-        player1: player1Socket,
-        player2: player2Socket,
-        whoseTurn: player1Socket.id,
+      games[gameId] = {
+        player1: player1.id,
+        player2: player2.id,
+        whoseTurn: player1.id,
         sign: {
-          [player1Socket.id]: 'X',
-          [player2Socket.id]: 'O',
+          [player1.id]: 'X',
+          [player2.id]: 'O',
         },
         playboard: [
           ['', '', ''],
@@ -132,161 +182,50 @@ io.on('connection', client => {
         ],
         gameStatus: 'ongoing', // "ongoing","won","draw"
         gameWinner: null, // winner_id if status won
-        winningCombination: [],
       }
 
-      games.push(gameData)
+      io.sockets.sockets.get(player1.id).join(gameId)
+      io.sockets.sockets.get(player2.id).join(gameId)
 
-      io.sockets.sockets.get(player1Socket.id).join(gameId)
-      io.sockets.sockets.get(player2Socket.id).join(gameId)
-
-      const response = []
-
-      sockets.forEach(socket => {
-        if (
-          socket.id !== player1Socket.id &&
-          socket.id !== player2Socket.id &&
-          socket.id !== client.id &&
-          !socket.isPlaying
-        ) {
-          response.push({
-            id: socket.id,
-            name: socket.playerData.name,
-            played: socket.playerData.played,
-            won: socket.playerData.won,
-            draw: socket.playerData.draw,
-          })
-        }
-      })
-
-      io.emit('excludePlayers', response)
+      io.emit('excludePlayers', { player1: player1.id, player2: player2.id })
       io.to(gameId).emit('gameStarted', {
-        status: true,
         gameId,
-        gameData,
+        ...games[gameId],
       })
     }
 
-    const response = {
-      status: false,
+    client.emit('alreadyPlaying', {
       message: 'Opponent is playing with someone else.',
-    }
-
-    client.emit('alreadyPlaying', response)
+    })
   })
 
   client.on('selectCell', data => {
-    const game = gameRules(data, players, games)
-
-    if (game.gameStatus !== 'draw' || game.gameStatus !== 'won') {
-      io.to(data.gameId).emit('selectCellResponse', game)
-    }
-
-    if (game.gameStatus === 'draw' || game.gameStatus === 'won') {
-      io.to(data.gameId).emit('selectCellResponse', game)
-
-      let gameBetweenSeconds = 10
-      const gameBetweenInterval = setInterval(() => {
-        gameBetweenSeconds--
-        io.to(data.gameId).emit('gameInterval', gameBetweenSeconds)
-
-        const gameIndex = games.indexOf(game)
-
-        if (gameIndex >= 0) {
-          games.splice(gameIndex, 1)
-        }
-
-        if (gameBetweenSeconds === 0) {
-          clearInterval(gameBetweenInterval)
-
-          const gameId = uuid()
-
-          game.player1.is_playing = true
-          game.player2.is_playing = true
-
-          game.player1.gameId = gameId
-          game.player2.gameId = gameId
-
-          game.player1.played += 1
-          game.player2.played += 1
-
-          const gameData = {
-            gameId,
-            player1: game.player1,
-            player2: game.player2,
-            whoseTurn: game.player1.id,
-            sign: {
-              [game.player1.id]: 'X',
-              [game.player2.id]: 'O',
-            },
-            playboard: [
-              ['', '', ''],
-              ['', '', ''],
-              ['', '', ''],
-            ],
-            gameStatus: 'ongoing', // "ongoing","won","draw"
-            gameWinner: null, // winner_id if status won
-            winningCombination: [],
-          }
-
-          games.push(gameData)
-
-          io.sockets.sockets.get(game.player1.id).join(gameId)
-          io.sockets.sockets.get(game.player2.id).join(gameId)
-
-          io.to(gameId).emit('nextGameData', {
-            status: true,
-            game_id: gameId,
-            game_data: games[gameId],
-          })
-          io.sockets.sockets.get(game.player1.id).leave(data.gameId)
-          io.sockets.sockets.get(game.player2.id).leave(data.gameId)
-          // delete game
-        }
-      }, 1000)
-    }
+    // TODO
   })
 
   client.on('disconnect', () => {
     console.log('disconnect : ' + client.id)
 
-    const existingSocket = sockets.find(({ id }) => id === client.id)
-    const indexSocket = sockets.indexOf(existingSocket)
+    if (typeof sockets[client.id] !== 'undefined') {
+      if (sockets[client.id].is_playing) {
+        io.to(sockets[client.id].gameId).emit('opponentLeft', {})
 
-    if (existingSocket) {
-      if (existingSocket.is_playing) {
-        io.to(existingSocket.gameId).emit('opponentLeft', {})
+        players[sockets[games[sockets[client.id].game_id].player1].name]
+          .played--
 
-        const player = players.find(({ id }) => client.id === id)
-        const playerIndex = players.indexOf(player)
+        players[sockets[games[sockets[client.id].game_id].player2].name]
+          .played--
 
-        let game = {}
-        let gameIndex = -1
-        let playerSocket = ''
-
-        if (playerIndex >= 0) {
-          game = games.find(({ gameId }) => gameId === existingSocket.gameId)
-          gameIndex = games.indexOf(game)
-          if (game) {
-            playerSocket =
-              game.player1.id === client.id ? game.player2.id : game.player1.id
-            const playerLeave =
-              game.player1.id === client.id ? game.player1 : game.player2
-
-            playerLeave.isPlaying = false
-          }
-          players.splice(playerIndex, 1)
-        }
-        if (playerSocket) {
-          io.sockets.sockets.get(playerSocket).leave(existingSocket.gameId)
-        }
-
-        games.splice(gameIndex, 1)
+        io.sockets.connected[
+          client.id === games[sockets[client.id].game_id].player1
+            ? games[sockets[client.id].game_id].player2
+            : games[sockets[client.id].game_id].player1
+        ].leave(sockets[client.id].game_id)
+        delete games[sockets[client.id].game_id]
       }
     }
-    if (indexSocket >= 0) {
-      sockets.splice(indexSocket, 1)
-    }
+
+    delete sockets[client.id]
     client.broadcast.emit('opponentDisconnected', {
       id: client.id,
     })
